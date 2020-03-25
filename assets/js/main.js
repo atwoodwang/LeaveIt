af8fa09a8723fca4597160adcde707ca2dd573ef
wangzicong <wangzicong@bytedance.com>
2020-03-25 19:03:35 +0800

    Some improvements

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,7 +22,7 @@ jQuery(function($) {
     _Blog.changeTitle = function() {
         var currentTitle = document.title;
         window.onblur = function() {
-            document.title = 'I miss you!（＞﹏＜）';
+            document.title = currentTitle;
         }
         window.onfocus = function() {
             document.title = currentTitle;
